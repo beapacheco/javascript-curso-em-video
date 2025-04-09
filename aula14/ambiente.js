6f365a3215a9faa42a23a9a8605f67f6f6c54e9f
Beatriz <beatriz.pacheco2010@hotmail.com>
2025-04-09 17:30:44 +0100

    exercicios de iterações

---
+++ aula14/ambiente.js
@@ -0,0 +1,29 @@
+console.log('Vai começar...')
+for (let c = 1; c <= 4; c++) {
+    console.log(c)
+}
+console.log('FIM') // aprendendo a usar DEBUG
+
+
+/* let c = 1
+while (c <= 5) {
+    console.log(c)
+    c++
+}
+
+------------------------------
+
+
+let c = 1
+do {
+    console.log(c)
+    c++
+} while (c <= 5)
+
+
+--------------------------------
+
+
+for (let c = 1;c <= 5;c++){
+    console.log(c)
+} */
