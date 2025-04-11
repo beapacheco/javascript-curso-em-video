0b70b36f6776988b806bd3a83e182515996df03a
Beatriz <beatriz.pacheco2010@hotmail.com>
2025-04-11 11:22:04 +0100

    ajustando o exercicio

--- testes/teste004/js/script.js
+++ testes/teste004/js/script.js
@@ -8,9 +8,9 @@ function verificar() {
     if (first == '' || second == '') {
         alert('Você deve preencher os campos')
     } else if (first > second) {
-        maioroumenor.innerHTML = `O número ${first} é maior que ${second}`
+        maioroumenor.innerHTML = `O primeiro número é maior que o segundo número`
     } else {
-        maioroumenor.innerHTML = `O número ${second} é maior que ${first}`
+        maioroumenor.innerHTML = `O segundo numero é maior que o primeiro número`
     } if (first == second) {
         maioroumenor.innerHTML = 'Os dois números são iguais'
     }
